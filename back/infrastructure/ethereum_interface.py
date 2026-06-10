31110b7ed8fceae1c9d79fa5a0f78177eb4042f0
Dan Kleczewski <dankleczew@gmail.com>
2026-06-10 15:44:10 +0200

    rajout

--- back/infrastructure/ethereum_interface.py
+++ back/infrastructure/ethereum_interface.py
@@ -1,35 +1,57 @@
+import json
+
 from web3 import Web3
 
 from infrastructure.config import CONFIG
 
 
-def create_contract() -> str:
-    pass
+# Common
+with open("Ticket.json") as fjson:
+        artifact = json.load(fjson)
+        
+ABI = artifact["abi"]
+BYTECODE = artifact["bytecode"]["object"]
+
+# ***
+# Functions
+# ***
+def create_contract(name: str, max_supply: int, ticket_price_in_eth: int) -> str:
+    w3 = Web3(Web3.HTTPProvider(CONFIG.rpc_url))
+
+    if not w3.is_connected():
+        raise Exception("Impossible de se connecter au noeud Ethereum")
+    
+    contract = w3.eth.contract(abi=ABI, bytecode=BYTECODE)
+
+    nonce = w3.eth.get_transaction_count(CONFIG.owner_address)
+
+    transaction = contract.constructor(name, "#", max_supply, "https://todo", ticket_price_in_eth).build_transaction(
+         {
+            'chainId': w3.eth.chain_id,
+            'gas': 3000000,
+            'maxFeePerGas': w3.to_wei('50', 'gwei'),
+            'maxPriorityFeePerGas': w3.to_wei('1.5', 'gwei'),
+            'nonce': nonce
+        }
+    )
+
+    signed_txn = w3.eth.account.sign_transaction(transaction, private_key=CONFIG.owner_private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.raw_transaction)
+    receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    return receipt.contractAddress
+
+
 
 def mint(address_contract: str, quantity: int, address_to: str):
-    RPC_URL = CONFIG.rpc_url
-    w3 = Web3(Web3.HTTPProvider(RPC_URL))
+    
+    w3 = Web3(Web3.HTTPProvider(CONFIG.rpc_url))
 
     if not w3.is_connected():
         raise Exception("Impossible de se connecter au noeud Ethereum")
 
 
-    contract_abi = [
-        {
-            "inputs": [
-                {"internalType": "address", "name": "to", "type": "address"},
-                {"internalType": "uint256", "name": "quantity", "type": "uint256"}
-            ],
-            "name": "mint",
-            "outputs": [
-                {"internalType": "uint256[]", "name": "", "type": "uint256[]"}
-            ],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ]
-
-    contract = w3.eth.contract(address=address_contract, abi=contract_abi)
+    contract = w3.eth.contract(address=address_contract, abi=ABI)
 
     nonce = w3.eth.get_transaction_count(CONFIG.owner_address)
 
